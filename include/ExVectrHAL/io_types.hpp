0c123f40fe55b529f63f3fe9963ce3326b6352db
ChSt15 <chrissteffen98@gmail.com>
2023-10-12 10:11:19 +0200

    Added touch IO type

--- include/ExVectrHAL/io_types.hpp
+++ include/ExVectrHAL/io_types.hpp
@@ -11,7 +11,7 @@ namespace VCTR
          * @brief IO types. Type of IO e.g. SPI, I2C UART etc.
          */
         enum class IO_TYPE_t
-        {   
+        {
             BUS_I2C = 0,
             BUS_SPI,
             BUS_UART,
@@ -21,12 +21,13 @@ namespace VCTR
             ANALOG_DAC,
             ANALOG_ADC,
             ANALOG_HBRIDGE,
+            ANALOG_TOUCH,
             DIGITAL_I,
             DIGITAL_O,
-            //Topic used as base. Usually for testing using topicIO in ExVectrNetwork.
+            // Topic used as base. Usually for testing using topicIO in ExVectrNetwork.
             TOPIC,
-            //Used to signify unknown/special types. Anything equal or greater than this is some other type
-            OTHER //KEEP AT END.
+            // Used to signify unknown/special types. Anything equal or greater than this is some other type
+            OTHER // KEEP AT END.
         };
     }
 
